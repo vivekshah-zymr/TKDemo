d25ca365544c48a0447e933ab331b5ac410a0d00
Vivek Shah <vivek.shah@zymr.com>
2018-01-08 18:48:28 +0530

    Change in Map.xaml file

--- TKDemo/MapPage.xaml.cs
+++ TKDemo/MapPage.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
 using Xamarin.Forms;
+using TK.CustomMap;
 
 namespace TKDemo
 {
@@ -10,6 +11,22 @@ namespace TKDemo
         public MapPage()
         {
             InitializeComponent();
+            mapViewTK.MapRegion = MapSpan.FromCenterAndRadius(new Position(23.066138, 72.535848), Distance.FromKilometers(0.4));
+            mapViewTK.Pins = new List<TKCustomMapPin>(new[]
+            {
+               new TKCustomMapPin{Title = "Home Location",Position = new Position(23.066138, 72.535848),ShowCallout = true}
+            });
         }
+
+        void Handle_MapClicked(object sender, TK.CustomMap.TKGenericEventArgs<TK.CustomMap.Position> e)
+        {
+            Debug.WriteLine("Handle_MapClicked");
+        }
+
+        void Handle_CalloutClicked(object sender, TK.CustomMap.TKGenericEventArgs<TK.CustomMap.TKCustomMapPin> e)
+        {
+            Debug.WriteLine("Handle_CalloutClicked");
+        }
+
     }
 }
